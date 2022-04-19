e92b98f4ad10cb274e24beccb9bd027362eef1df
Alex Yang <ajyang2009@gmail.com>
2022-04-19 15:18:19 -0400

    connects action and data stream

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 
-import { catchError, Observable, of, Subscription } from 'rxjs';
+import { BehaviorSubject, catchError, combineLatest, EMPTY, filter, map, Observable, of, startWith, Subject, Subscription } from 'rxjs';
 import { ProductCategory } from '../product-categories/product-category';
+import { ProductCategoryService } from '../product-categories/product-category.service';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
@@ -9,23 +15,64 @@ import { ProductService } from './product.service';
 @Component({
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductListComponent{
+export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
-  categories: ProductCategory[] = [];
+  // selectedCategoryId = 1;
 
-  products$= this.productService.productsWithCategory$ //: Observable<Product[]> | undefined =
-        .pipe(
-          catchError(err => {
-            this.errorMessage = err;
-            return of([]);
-          })
-        );
+  private categorySelectedSubject = new BehaviorSubject<number>(0); //initialize with number 0, otherwise 'new Subject<number>()'
+  categorySelectedAction$ = this.categorySelectedSubject.asObservable(); // expose the subject as an observ'able
+
+  products$ = combineLatest([ //combine action with data stream
+    this.productService.productsWithCategory$, //want the category string, not id
+    this.categorySelectedAction$
+    .pipe(
+      startWith(0) //initializes with number 0, same as behavior subject
+    )
+  ])
+    .pipe( //perform filter
+      map(([products, selectedCategoryId]) =>  //structure array elements, [products, cat id from user]
+      products.filter(product =>
+        selectedCategoryId ? product.categoryId === selectedCategoryId: true //simple filter
+    )),
+    catchError(err => {
+      this.errorMessage = err;
+      return EMPTY;
+    })
+    );
+
+  // products$ = this.productService.productsWithCategory$ //: Observable<Product[]> | undefined =
+  //   .pipe(
+  //     catchError((err) => {
+  //       this.errorMessage = err;
+  //       return of([]);
+  //     })
+  //   );
   //sub!: Subscription;
 
-  constructor(private productService: ProductService) { }
+  categories$ = this.productCategoryService.productCategories$.pipe(
+    catchError((err) => {
+      this.errorMessage = err;
+      return of([]);
+    })
+  );
+
+  // productsSimpleFilter$ = this.productService.productsWithCategory$.pipe(
+  //   map((products) =>
+  //     products.filter((product) =>
+  //       this.selectedCategoryId
+  //         ? product.categoryId === this.selectedCategoryId
+  //         : true
+  //     )
+  //   ) //check if selected category exists, if it does compare, if not return true to select call
+  // );
+
+  constructor(
+    private productService: ProductService,
+    private productCategoryService: ProductCategoryService
+  ) {}
 
   // ngOnInit(): void {
   //   this.products$ = this.productService.getProducts()
@@ -50,6 +97,7 @@ export class ProductListComponent{
   }
 
   onSelected(categoryId: string): void {
-    console.log('Not yet implemented');
+    // this.selectedCategoryId = +categoryId
+    this.categorySelectedSubject.next(+categoryId);//called each time the user selects item from dropdown
   }
 }
